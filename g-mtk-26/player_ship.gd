7c1b90605d6b9c1ec4b7ce44228133776cff2796
TalesOfXanthar <whitmantgarrity@gmail.com>
2026-07-22 16:33:00 -0700

    Added Player Scene
    Added the player scene and movement for it, numbers could be tweaked but basic movement down

---
+++ g-mtk-26/player_ship.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+var rotation_velocity := 0.0
+
+func _physics_process(delta: float) -> void:
+	var rotation_direction := 0.0
+	var movement_direction := 0.0
+	
+	rotation_direction = Input.get_axis("rotate_counterclockwise", "rotate_clockwise")
+	if rotation_direction != 0.0:	
+		rotation_velocity += lerpf(rotation_velocity, rotation_direction * 0.05, 0.02)
+		rotation_velocity = clampf(rotation_velocity, -0.07, 0.07)
+	else:
+		rotation_velocity = 0
+	rotation += rotation_velocity
+
+	movement_direction = Input.get_axis("move_backwards", "move_forward")
+	if movement_direction != 0.0:
+		var movement_vector = transform.x * movement_direction * 60
+		velocity = velocity.move_toward(movement_vector, 5)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, 0.5)
+		
+	move_and_slide()
