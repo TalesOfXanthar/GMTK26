eb17c0f5e0790602aa53552cf2c41417cfdec018
The-Linker15 <146147008+The-Linker15@users.noreply.github.com>
2026-07-26 08:43:32 -0700

    Main menu

---
+++ g-mtk-26/scenes/main_menu.gd
@@ -0,0 +1,23 @@
+extends TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_credits_pressed() -> void:
+	$ColorRect.show()
+
+
+func _on_exit_credits_pressed() -> void:
+	$ColorRect.hide()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
